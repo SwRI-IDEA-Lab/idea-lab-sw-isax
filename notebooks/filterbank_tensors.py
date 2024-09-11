523953868c1a321dedf8a7f7ebd325ab5bc79a1a
rokkaan5 <rokkakobayashi@msn.com>
2024-09-11 16:01:22 -0600

    finish last couple steps

--- notebooks/filterbank_tensors.py
+++ notebooks/filterbank_tensors.py
@@ -2,6 +2,9 @@
 import torch
 import datetime as dt
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 # local imports
 import os
 _FILE_DIR = os.path.dirname( os.path.abspath(__file__))
@@ -12,7 +15,6 @@ import ws_optimization.sw_dataset as swd
 import fdl21.data.filterbanks as fb
 # %% [markdown] ==================================================================================================
 # ## Get 1D signal from dataloader 
-
 # %% 1D signal from dataloader ----------------------------------------------------------------------------------
 data_load = swd.SolarWindDataset(dir_path = 'C:/Users/jkobayashi/gh_repos/idea-lab-sw-isax/sw-data/nasaomnireader/1999')
 signal_1d = data_load.__getitem__(10)
@@ -21,16 +23,14 @@ signal_1d
 # %% [markdown] ==================================================================================================
 # ## Apply fourier transform using PyTorch FFT 
 # (should return 1D tensor)
-
  # %% Fourier transform ------------------------------------------------------------------------------------------
-sig_fft = torch.fft.fft(signal_1d)
+sig_fft = torch.fft.fft(signal_1d)# - torch.mean(signal_1d))
 print(sig_fft)
 sig_fft = torch.real(sig_fft)
 print(sig_fft)
 
 # %% [markdown] ==================================================================================================
 # ## Create a 1D tensor with frequencies of triangle apexes 
-
 # %% Filterbanks (from local filterbank module)------------------------------------------------------------------
 fbank = fb.filterbank(restore_from_file='C:/Users/jkobayashi/gh_repos/idea-lab-sw-isax/data/filterbanks/fb_0.01_1.66_3.33_5.0_DC_HF.pkl')
 fbank.visualize_filterbank()
@@ -45,16 +45,31 @@ freq = torch.tensor(fbank.fftfreq)      # 1D tensor of frequencies
 # (Dimensions = n_filters x length(fourier_spectrum))
 
 # %%
-
+all_filtered_signals = torch.empty(mat.shape[0], signal_1d.shape[0])
 
 # %% [markdown] ==================================================================================================
 # ## Loop over apexes, multiply triangle frequencies and multiply by fourier spectrum
-# Place result in 2D repository tensor
+# Place result in 2D repository tensor 
+# 
+# Inverse Fourier transform on each row and add to get original signal
+
 
 # %%
+sample_freq = torch.fft.fftfreq(signal_1d.shape[0],d=1/60)
+total_sig = torch.zeros(signal_1d.shape[0])
+for i,bank in enumerate(mat):
+    filter = np.interp(np.abs(sample_freq),freq,bank,left=None,right=None,period=None)
+    filteredYF = sig_fft.clone()
+    filteredYF = filteredYF*filter
 
+    filtered_sig = torch.real(torch.fft.ifft(filteredYF))
 
-# %% [markdown] ==================================================================================================
-# ## Inverse Fourier transform on each row and add to get original signal
+    all_filtered_signals[i] = filtered_sig
+    total_sig += filtered_sig
 
 # %%
+fig, axes = plt.subplots(figsize=(8,5))
+axes.plot(signal_1d,label='Original Signal')
+axes.plot(total_sig,label='Total Filtered Signal')
+axes.legend()
+plt.show()
