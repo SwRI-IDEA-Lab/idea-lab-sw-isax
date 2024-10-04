0b45cdc0ee282858213c51750fc648938459d927
Jasmine Kobayashi <jasmine.kobayashi@swri.org>
2024-10-04 12:02:00 -0600

    change to jupyter notebook like appearance (code chunks in script via '#%%') and utilize get_test_data from build_filterbanks code instead of duplicating in the script itself
    also adjusted window calculation to take total_seconds into account
    
    AND incorporated comparison to smoothing in time_chunking module

--- notebooks/fourier_smoothing_detrending.py
+++ notebooks/fourier_smoothing_detrending.py
@@ -1,4 +1,4 @@
-
+# %% libraries
 import cdflib
 
 from tqdm import tqdm
@@ -43,132 +43,62 @@ import fdl21.data.prototyping_metrics as pm
 import fdl21.utils.time_chunking as tc
 import fdl21.data.build_filterbanks as fb
 
-_PSP_MAG_DATA_DIR = '/sw-data/psp/mag_rtn/'
-_WIND_MAG_DATA_DIR = '/sw-data/wind/mfi_h2/'
-_OMNI_MAG_DATA_DIR = '/sw-data/nasaomnireader/'
-_SRC_DATA_DIR = os.path.join(_SRC_DIR,'data',)
-
-_EXPONENTS_LIST = [2.15, 1.05, 1.05]
-
-def get_test_data(fname_full_path=None,
-                  fname = None,
-                  instrument = 'omni',
-                  start_date = dt.datetime(year=2019,month=5,day=15,hour=0),
-                  end_date = dt.datetime(year=2019,month=5,day=16,hour=0),
-                  rads_norm=True,
-                  orbit_fname = None):
-    """Retrieve a set of data to test and visualize filterbank application
-    
-    Parameters
-    ----------
-    fname_full_path : string
-        complete file path to cdf file to extract data from
-        A value for fname_full_path or fname (but not both) is required 
-    fname : string
-        part-way path to cdf file to extract data from, after 
-        the selected "_DATA_DIR" that is selected by indicated instrument.
-        A value for fname_full_path or fname (but not both) is required
-    start_date: datetime, optional
-        test data start time 
-    end_date: datetime, optional
-        test data end time
-    rads_norm : bool, optional
-        Boolean flag for controlling the normalization of the magnetic field 
-        to account for the decay of the field strength with heliocentric distance
-    orbit_fname : string, optional
-        file path to psp orbit data
-    """
-    if fname_full_path is None:
-        if instrument == 'psp':
-            data_dir = _PSP_MAG_DATA_DIR
-        elif instrument=='wind' :
-            data_dir = _WIND_MAG_DATA_DIR
-        elif instrument == 'omni':
-            data_dir = _OMNI_MAG_DATA_DIR
-
-        assert fname is not None, "Need to provide value for fname or fname_full_path"
-        # Generate the full path to the file
-        fname_full_path = os.path.join(
-            _SRC_DIR + data_dir,
-            *fname.split('/') # this is required do to behavior of os.join
-        )
-        
-    if instrument == 'psp':
-        if orbit_fname is not None:
-            orbit_fname = os.path.join(
-            _SRC_DATA_DIR,
-            orbit_fname)
-            # orbit dataframe
-            orbit = pd.read_csv(
-                orbit_fname,
-                sep=",",
-                comment ="#",
-                index_col='EPOCH_yyyy-mm-ddThh:mm:ss.sssZ',
-                parse_dates=['EPOCH_yyyy-mm-ddThh:mm:ss.sssZ'],
-            )
-        mag_df = pm.read_PSP_dataset(
-            fname=fname_full_path,
-            orbit=orbit,
-            rads_norm=rads_norm,
-            exponents_list=_EXPONENTS_LIST
-        )
-    elif instrument == 'wind':
-        mag_df = pm.read_WIND_dataset(
-            fname=fname_full_path
-        )
-    elif instrument == 'omni':
-        mag_df = pm.read_OMNI_dataset(
-            fname=fname_full_path
-        )
-    mag_df.interpolate(inplace=True)
-    mag_df = mag_df[start_date:end_date]
-
-
-    return mag_df
-
+# %%
 def moving_avg_freq_response(f,window=dt.timedelta(minutes=3000),cadence=dt.timedelta(minutes=1)):
     # TODO: Concern = does centered windows change the response?
-    window_size = int(window/cadence)
+    window_size = int(window.total_seconds()/cadence.total_seconds())
     numerator = np.sin(np.pi*f*window_size)
     denominator = window_size*np.sin(np.pi*f)
     return abs(numerator/denominator)
 
+# %% Get data
+year = '2019'
+month = '05'
+test_cdf_file_path =_SRC_DIR+fb._OMNI_MAG_DATA_DIR+ year +'/omni_hro_1min_'+ year+month+'01_v01.cdf'
 
-if __name__ == '__main__':
+mag_df = fb.get_test_data(fname_full_path=test_cdf_file_path)
+mag_df
 
-    # Get data
-    year = '2019'
-    month = '05'
-    test_cdf_file_path =_SRC_DIR+_OMNI_MAG_DATA_DIR+ year +'/omni_hro_1min_'+ year+month+'01_v01.cdf'
+# %% FT
+cadence = dt.timedelta(seconds=60)
+cols = ['BY_GSE']
 
-    mag_df = get_test_data(fname_full_path=test_cdf_file_path)
+mag_df=mag_df[cols]
+mag_df.sort_index(inplace=True)
+mag_df.interpolate(method='index', kind='linear',limit_direction='both',inplace=True)
+df_index=pd.date_range(start=mag_df.index[0], end=mag_df.index[-1], freq=cadence)
 
-    # FT
-    cadence = dt.timedelta(seconds=60)
-    cols = ['BY_GSE']
-
-    mag_df=mag_df[cols]
-    mag_df.sort_index(inplace=True)
-    mag_df.interpolate(method='index', kind='linear',limit_direction='both',inplace=True)
-    df_index=pd.date_range(start=mag_df.index[0], end=mag_df.index[-1], freq=cadence)
-
-    sig_fft_df = fft.fftn(mag_df - mag_df.mean(),axes=0)
-
-    # plot frequency response
-    fb = fb.filterbank()
-    fb.build_triangle_fb(num_fft_bands=int(mag_df.shape[0])+1,sample_rate=1/60) # triangle filters are not necessary; I just wanted to make an fftfreq spectrum
-
-    plt.plot(fb.fftfreq,abs(moving_avg_freq_response(f=fb.fftfreq,)))
-    plt.show()
-
-    # Apply moving average "filter"
-    Y = sig_fft_df.ravel()*moving_avg_freq_response(f=fb.fftfreq[1:])
-    filtered_y = np.real(fft.ifft(Y))
-
-    plt.plot(mag_df.index,filtered_y)
-    plt.show()
-
-    plt.plot(mag_df.index,mag_df[:])
+sig_fft_df = fft.fftn(mag_df - mag_df.mean(),axes=0)
 
 
+# Smoothing filter
+FB = fb.filterbank()
+FB.build_triangle_fb(num_fft_bands=int(mag_df.shape[0])+1,sample_rate=1/60) # triangle filters are not necessary; I just wanted to make an fftfreq spectrum
 
+window = dt.timedelta(seconds=18000)
+smoothing_ft = moving_avg_freq_response(f=FB.fftfreq[1:],
+                                        window=window,
+                                        cadence=cadence)
+# %% plot frequency response
+plt.plot(FB.fftfreq[1:],smoothing_ft)
+plt.show()
+
+# %% Apply moving average "filter"
+Y = sig_fft_df.ravel()*smoothing_ft
+filtered_y = np.real(fft.ifft(Y))
+
+# %% smoothing in time chunking
+smooth_y = tc.preprocess_smooth_detrend(mag_df=mag_df-mag_df.mean(),
+                                        cols=cols,
+                                        detrend_window=dt.timedelta(seconds=0),
+                                        smooth_window=window)
+
+# %% Compare
+plt.plot(mag_df.index,mag_df[:]-mag_df.mean(),label='original')
+plt.plot(mag_df.index,filtered_y,label='smoothing via Fourier')
+plt.plot(mag_df.index,smooth_y,label='original smoothing')
+plt.title(f'Fourier application of smoothing: window = {int(window.total_seconds())} seconds')
+plt.legend()
+plt.show()
+
+# %%
